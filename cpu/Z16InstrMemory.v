22b90ee224b69a2eaabe1428e13a8a0ec87d309f
shun74 <syunnsyunn74@gmail.com>
2023-09-22 19:59:36 +0900

    LOAD operation implemented

--- cpu/Z16InstrMemory.v
+++ cpu/Z16InstrMemory.v
@@ -3,13 +3,13 @@ module Z16InstrMemory(
   output wire [15:0] o_instr
 );
 
-  wire [15:0] i_mem[4:0];
-  assign o_instr = i_mem[i_addr[15:1]];
+  wire [15:0] mem[4:0];
+  assign o_instr = mem[i_addr[15:1]];
 
-  assign i_mem[0] = 16'h0A19;
-  assign i_mem[1] = 16'h1220;
-  assign i_mem[2] = 16'hFF19;
-  assign i_mem[3] = 16'hFC4F;
-  assign i_mem[4] = 16'h00FD;
+  assign mem[0] = 16'h406A; // LOAD 4 ZR G2 
+  assign mem[1] = 16'h0000;
+  assign mem[2] = 16'h0000;
+  assign mem[3] = 16'h008A; // LOAD 0 ZR G4
+  assign mem[4] = 16'h0000;
 
 endmodule
