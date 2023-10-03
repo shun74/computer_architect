d53238942255ff6c02a5ff187f0a570f366a1fc6
shun74 <syunnsyunn74@gmai.com>
2023-10-04 01:30:15 +0900

    led button implemented

--- cpu/Z16CPU_tb.v
+++ cpu/Z16CPU_tb.v
@@ -2,9 +2,11 @@ module Z16CPU_tb;
 
   reg i_clk = 1'b0;
   reg i_rst = 1'b0;
+  reg i_button = 1'b0;
+  wire [5:0] o_led;
 
   always #1 begin
-    i_clk = ~i_clk;
+    i_clk <= ~i_clk;
   end
 
   initial begin
@@ -13,16 +15,22 @@ module Z16CPU_tb;
   end
 
   Z16CPU CPU(
-    .i_clk (i_clk),
-    .i_rst (i_rst)
+    .i_clk      (i_clk      ),
+    .i_rst      (i_rst      ),
+    .i_button   (i_button   ),
+    .o_led      (o_led      )
   );
 
   initial begin
-    i_rst = 1'b1;
+    i_rst   = 1'b1;
     #2
-    i_rst = 1'b0;
+    i_rst   = 1'b0;
+    #100
+    i_button = 1'b1;
+    #20
+    i_button = 1'b0;
     #100
     $finish;
   end
-  
+
 endmodule
